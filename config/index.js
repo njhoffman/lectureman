873e1bf62bad8178f62321cb4c276396a22f3188
Nicholas Hoffman <njhoffman1982@gmail.com>
2020-08-13 10:14:52 -0500

    initial commit

---
+++ config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  searchUrl: 'http://thegreatcourses.com/search/sort-by/relevance/sort-direction/desc/mode/list',
+  scrapeUrl:
+    'http://www.thegreatcourses.com/courses/types/individual-courses/sort-by/name/sort-direction/asc/mode/list'
+};
